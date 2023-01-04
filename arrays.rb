3bf71ddb024d4afd2d75869bacacae040402fb4a
Logan Wilson <loganwilson777@gmail.com>
2023-01-04 15:01:22 -0700

    call 4 arrays

--- arrays.rb
+++ arrays.rb
@@ -3,3 +3,10 @@ array = ["jake","beth","katelyn","wyatt"]
 array_2 = [5,10,15,20]
 array_3 = [5.5,10.5,15.5,20.5]
 array_4 = [true,true,false,false]
+
+#When calling on these arrays, the first item in each array will always be 0, in order to call on the 4th item in the array, you would need to actually write `3` in the code in order to get the desired reponse.
+
+puts array
+puts array_2[2]
+puts array_3[0]
+puts array_4[3]

de39bcd48e2a38516af61562a16ca21da022db32
Logan Wilson <loganwilson777@gmail.com>
2023-01-04 15:06:44 -0700

    comment on index positions

--- arrays.rb
+++ arrays.rb
@@ -4,9 +4,11 @@ array_2 = [5,10,15,20]
 array_3 = [5.5,10.5,15.5,20.5]
 array_4 = [true,true,false,false]
 
-#When calling on these arrays, the first item in each array will always be 0, in order to call on the 4th item in the array, you would need to actually write `3` in the code in order to get the desired reponse.
+#This method of calling the arrays will result in displaying the data in the arrays. as for the frist array, it will display all of the data in it, while the others will display a certain part of the array.
 
 puts array
 puts array_2[2]
 puts array_3[0]
 puts array_4[3]
+
+#When calling on these arrays, the first item in each array will always be 0, in order to call on the 4th item in the array, you would need to actually write `3` in the code in order to get the desired reponse.

e4f4972c17a6217b865d47e6a7824252f0e79e61
Logan Wilson <loganwilson777@gmail.com>
2023-01-04 22:30:10 -0700

    Add missing work

--- arrays.rb
+++ arrays.rb
@@ -6,10 +6,10 @@ array_4 = [true,true,false,false]
 
 #This method of calling the arrays will result in displaying the data in the arrays. as for the frist array, it will display all of the data in it, while the others will display a certain part of the array.
 
-puts array
-puts array_2[2]
-puts array_3[0]
-puts array_4[3]
+# puts array
+# puts array_2[2]
+# puts array_3[0]
+# puts array_4[3]
 
 #When calling on these arrays, the first item in each array will always be 0, in order to call on the 4th item in the array, you would need to actually write `3` in the code in order to get the desired reponse.
 
@@ -21,3 +21,15 @@ c = a + [4,5]
 
 puts a
 puts c
+
+# These are the 4 methods we learned today,
+
+puts array_2 
+# push adds data at the beggining of the array
+puts array_2.push(25)
+# unshift adds data at the beggining of the array
+puts array_2.unshift(0)
+# shift removes data at the beggining of the array
+puts array_2.shift
+# pop removes data at the end of the array
+puts array_2.pop
